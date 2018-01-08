2691370f4eca6ef52c4a59ae7663c5855f0279a5
Mr. Rosenfeld <srosenfeld@users.noreply.github.com>
2018-01-08 08:52:53 -0500

    Added import time & changed print

--- madlibs_kate.py
+++ madlibs_kate.py
@@ -1,3 +1,4 @@
+import time
 print("Write a verb ending is s")
 verb1 = input()
 
@@ -13,4 +14,6 @@ time1 = input()
 
 print("Every morning Kendall " + verb1 + " during the day, she decides to ride a "
       + adjnoun1 + " at exactly " + time1 + " Kendall takes a " + clothing1 +
-" and eats it. Kendall realizes she needs a job, sadly no normal jobs want her so she is hired to be a " + weirdjob1 +
+" and eats it. Kendall realizes she needs a job, sadly no normal jobs want her so she is hired to be a " + weirdjob1)
+
+time.sleep(30)
