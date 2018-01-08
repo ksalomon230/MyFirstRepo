6727c30f4d16980e592494453ac5a7b508cb36a4
ksalomon230 <35225785+ksalomon230@users.noreply.github.com>
2018-01-08 08:48:55 -0500

    Add files via upload

---
+++ madlibs_kate.py
@@ -0,0 +1,16 @@
+print("Write a verb ending is s")
+verb1 = input()
+
+print("Write an adjective then a noun")
+adjnoun1 = input()
+
+print("Write a a piece of clothing")
+clothing1 = input()
+
+print("write an exact time")
+time1 = input()
+### MAD LIB ###
+
+print("Every morning Kendall " + verb1 + " during the day, she decides to ride a "
+      + adjnoun1 + " at exactly " + time1 + " Kendall takes a " + clothing1 +
+" and eats it. Kendall realizes she needs a job, sadly no normal jobs want her so she is hired to be a " + weirdjob1 +
